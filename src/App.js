2bd047f554fc024ba5fb76bad9eef09153162d63
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2019-07-09 11:19:51 -0500

    added state functionality

--- src/App.js
+++ src/App.js
@@ -10,35 +10,36 @@ import characters from './characters.json'
 import ImgContainer from './components/ImageContainer';
 
 class App extends Component {
-  state = {
-    characters,
-    score: 0,
-    topScore: 0
+  constructor() {
+    super()
+    this.state = { characters, score: 0, topScore: 0 }
+  }
+
+  handleIncrement = () => {
+    this.setState({ score: this.state.score + 1 });
   };
 
   render() {
     return (
       <div className="App">
-        <Section>
-          <Hero color="info">
-            <Hero.Body>
-              <Container>
-                <Heading>The Office Clicky Game</Heading>
-                <Columns>
-                  <Columns.Column size={6}>Click an image to begin!</Columns.Column>
-                  <Columns.Column size={6}>Score: {this.state.score} | Top Score: {this.state.topScore}</Columns.Column>
-                </Columns>
-              </Container>
-            </Hero.Body>
-          </Hero>
-        </Section>
+        <Hero color="info">
+          <Hero.Body>
+            <Container>
+              <Heading>The Office Clicky Game</Heading>
+              <Columns>
+                <Columns.Column size={6}>Click an image to begin!</Columns.Column>
+                <Columns.Column size={6}>Score: {this.state.score} | Top Score: {this.state.topScore}</Columns.Column>
+              </Columns>
+            </Container>
+          </Hero.Body>
+        </Hero>
         <Section>
           <Container>
             <Columns>
               {this.state.characters.map(character => (
                 <ImgContainer
-                onClick={this.setState.score + 1}
-                key={character.id}
+                  function={() => this.handleIncrement()}
+                  key={character.id}
                   width="150px"
                   alt={character.name}
                   image={character.image}
--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Columns } from 'react-bulma-components';
 import { Card } from 'react-bulma-components';
 
-
-function ImgContainer(props) {
-  return (
-    <Columns.Column size={2}>
-      <Card style={{width: props.width}}>
-        <Card.Image src={props.image}></Card.Image>
-      </Card>
-    </Columns.Column>
-  );
+class ImgContainer extends Component {
+  render() {
+    return (
+      <Columns.Column size={2}>
+        <Card onClick={this.props.function} style={{ width: this.props.width }}>
+          <Card.Image src={this.props.image}></Card.Image>
+        </Card>
+      </Columns.Column>
+    );
+  }
 }
-
 
 export default ImgContainer;
